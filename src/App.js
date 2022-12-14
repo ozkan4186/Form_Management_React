05fac8a182478aa85fa6043d5147074e17df62ef
özkan demir <ozkanndemir2009@gmail.com>
2022-12-14 12:31:27 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div className="App">
+      <AppRouter/>
       <ToastContainer />
     </div>
   ); 
